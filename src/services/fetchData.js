02a58b02524656a05849cd0bbc3c498d5860eeed
Olugbenga5 <108488902+Olugbenga5@users.noreply.github.com>
2025-06-18 17:02:04 +0100

    new file:   .env 	modified:   package-lock.json 	modified:   package.json 	new file:   src/components/HorizontalScrollbar.js 	modified:   src/components/SearchExercises.js 	new file:   src/services/fetchData.js

---
+++ src/services/fetchData.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+    'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+  },
+};
+
+
+export const fetchWithAxios = async (url, options) => {
+  try {
+    const response = await axios.request({ url, ...options });
+    return response.data;
+  } catch (error) {
+    console.error('Axios fetch error:', error);
+    return null;
+  }
+};
+
+export const fetchData = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return null;
+  }
+};
